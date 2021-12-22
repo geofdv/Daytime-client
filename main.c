26cd94c7289f2e686186f2d6ff5da6203008f47d
George <fed.georg @ yandex dot ru>
2021-12-22 23:13:26 +0300

    server example from stevens

--- main.c
+++ main.c
@@ -1,13 +1,53 @@
+/*
+ * Send a UDP datagram to the daytime server on some other host.
+ * read the reply, and print the time and date on the server.
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <string.h>
 
-#define PROG_NAME
+#define BUFFSIZE 150
 
 int
 main(int argc, char *argv[])
 {
+	struct sockaddr_in serv;
+	char buff[BUFFSIZE];
+	int sockfd, n;
+	int numWritten;
+
+	sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) {
+		perror("socker error");
+		exit(1);
+	}
+
+	bzero((char *) &serv, sizeof(serv));
+	serv.sin_family = AF_INET;
+	serv.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv.sin_port = htons(13);
+
+	numWritten = sendto(sockfd, buff, BUFFSIZE, 0, (struct sockaddr *) &serv, sizeof(serv));
+	if (numWritten != BUFFSIZE) {
+		perror("sendo");
+		exit(2);
+	}
+
+	n = recvfrom(sockfd, buff, BUFFSIZE, 0, (struct sockaddr *) NULL, (socklen_t *) NULL);
+	if (n < 2) {
+		perror("recvfrom");
+		exit(2);
+	}
+
+	buff[n - 2] = 0;
+
+	printf("%s\n", buff);
+
 	return 0;
 }
