3f0744423f6c889b7a87c45679051d29fb4dec8c
George <fed.georg @ yandex dot ru>
2024-04-11 22:06:44 +0300

    refactoring

--- Makefile
+++ Makefile
@@ -1,23 +1,14 @@
 CC = gcc
-CFLAGS = -Wall -Werror -g -pedantic -DLINUX -D_GNU_SOURCE
-SRCMODULES = 
-OBJMODULES = $(SRCMODULES:.c=.o)
+CFLAGS = -std=c89 -Wall -Werror -g -pedantic -DLINUX -D_GNU_SOURCE
 
 %.o: %.c %.h
 	$(CC) $(CFLAGS) -c $< -o $@
 
-exe: main.c $(OBJMODULES)
+exe: main.c
 	$(CC) $(CFLAGS) $^ -o $@
 
 run: exe
 	./exe
-
-ifneq (clean, $(SRCMODULES))
--include deps.mk
-endif
-
-deps.mk: $(SRCMODULES)
-	$(CC) -MM $^ > $@
 
 clean:
 	rm -f *.o a.out
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
- * Send a UDP datagram to the daytime server on some other host.
- * read the reply, and print the time and date on the server.
+ * Send a UDP datagram to the daytime server on local host.
+ * Read the reply, and print the time and date on the server.
  */
 
 #include <sys/types.h>
@@ -12,15 +12,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct sockaddr_in sa_in;
+
 int
-main(int argc, char *argv[])
+main()
 {
-	enum { buffsize = 150 };
+	enum { buffsize = 256 };
 	char buff[buffsize];
 
 	struct sockaddr_in serv;
-	int sockfd, n;
-	int numWritten;
+	int sockfd;
+	int nsent, nreceived;
 
 	sockfd = socket(PF_INET, SOCK_DGRAM, 0);
 	if (sockfd < 0) {
@@ -33,19 +35,19 @@ main(int argc, char *argv[])
 	serv.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serv.sin_port = htons(13);
 
-	numWritten = sendto(sockfd, buff, buffsize, 0, (struct sockaddr *) &serv, sizeof(serv));
-	if (numWritten != buffsize) {
+	nsent = sendto(sockfd, buff, buffsize, 0, (sa_in *) &serv, sizeof(serv));
+	if (nsent != buffsize) {
 		perror("sendo");
 		exit(2);
 	}
 
-	n = recvfrom(sockfd, buff, buffsize, 0, (struct sockaddr *) NULL, (socklen_t *) NULL);
-	if (n < 2) {
+	nreceived = recvfrom(sockfd, buff, buffsize, 0, (sa_in *) NULL, (socklen_t *) NULL);
+	if (nreceived < 2) {
 		perror("recvfrom");
 		exit(2);
 	}
 
-	buff[n - 2] = 0;
+	buff[nreceived - 2] = 0;
 
 	printf("%s\n", buff);
 
